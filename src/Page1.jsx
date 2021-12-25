7aab9b8a5ecc5b1ea6903b067e5d1427b4868cdf
hekushi <78855499+hekushi@users.noreply.github.com>
2021-12-26 08:16:10 +0900

    add1226

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
   return (
     <div>
       <h1>Page1です</h1>
-      <Link to="/Page1/detailA">DetailA</Link>
+      <Link to={{ pathname: "/Page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/Page1/detailB">DetailB</Link>
     </div>
