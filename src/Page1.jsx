89876ce9c85af7ea4a32908e39291ec1dfc78818
hekushi <78855499+hekushi@users.noreply.github.com>
2021-12-27 07:33:43 +0900

    add1227end

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,14 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
   console.log(arr);
+
+  const history = useHistory();
+
+  const onClickDetailA = () => history.push("/Page1/detailA");
   return (
     <div>
       <h1>Page1です</h1>
       <Link to={{ pathname: "/Page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/Page1/detailB">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
